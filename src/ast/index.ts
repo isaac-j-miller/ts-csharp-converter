1f7c319f380baf7d8f95768c3a5c25b8ae9bd590
Isaac Miller <imiller@DCIMILLER.local>
2022-07-29 13:58:28 -0400

    clean up type factory

--- src/ast/index.ts
+++ src/ast/index.ts
@@ -9,17 +9,8 @@ import {
   TypeAliasDeclaration,
 } from "ts-morph";
 import { TypeRegistry } from "src/converter/registry";
-import {
-  TypeRegistryDictType,
-  TypeRegistryType,
-  TypeRegistryUnionType,
-} from "src/converter/registry-types";
 import { IRegistryType } from "src/converter/types";
-import {
-  asPrimitiveTypeName,
-  createSymbol,
-  getFinalSymbolOfType,
-} from "src/converter/util";
+import { TypeFactory } from "src/converter/type-factory";
 
 type DeclarationType =
   | EnumDeclaration
@@ -29,12 +20,14 @@ export class AstTraverser {
   private project: Project;
   private entrySourceFile: SourceFile;
   private registry: TypeRegistry;
+  private typeFactory: TypeFactory;
   constructor(entrypoint: string, tsconfigPath: string) {
     this.project = new Project({
       tsConfigFilePath: tsconfigPath,
     });
     this.entrySourceFile = this.project.getSourceFileOrThrow(entrypoint);
     this.registry = new TypeRegistry();
+    this.typeFactory = new TypeFactory(this.registry);
   }
   private processDeclaration<T extends DeclarationType>(node: T) {
     const name = node.getName();
@@ -47,200 +40,8 @@ export class AstTraverser {
     asType: Type,
     internal: boolean = false
   ): IRegistryType {
-    const typeAsPrimitive = asPrimitiveTypeName(asType);
-    if (typeAsPrimitive) {
-      return this.registry.getType(typeAsPrimitive)!;
-    }
-    const symbolToUse = createSymbol(name, asType);
-    if (asType.isUnion()) {
-      const unionTypes = asType.getUnionTypes();
-      const nonUndefinedUnionTypes = unionTypes.filter((u) => !u.isUndefined());
-      if (
-        nonUndefinedUnionTypes.every((unionType) => unionType.isStringLiteral())
-      ) {
-        const members = unionTypes.map((member) =>
-          member.getLiteralValueOrThrow()
-        );
-        const unionRegType = new TypeRegistryUnionType(
-          this.registry,
-          name,
-          symbolToUse,
-          members as string[],
-          internal,
-          asType
-        );
-        console.debug(`Adding union type ${name} to registry`);
-        this.registry.addType(unionRegType);
-        return unionRegType;
-      }
-      if (nonUndefinedUnionTypes.length === 1) {
-        return this.createType(name, node, nonUndefinedUnionTypes[0], internal);
-      }
-    }
-    const stringIndexType = asType.getStringIndexType();
-    if (stringIndexType) {
-      const valueType = stringIndexType.getApparentType();
-      const indexType = this.registry.getType("string")!.getSymbol();
-      const valueTypeName = `${name}Value`;
-      console.debug(`Creating internal type ${valueTypeName}`);
-      const vType = this.createType(valueTypeName, node, valueType, true);
-
-      const mappedType = new TypeRegistryDictType(
-        this.registry,
-        name,
-        symbolToUse,
-        indexType,
-        vType.getSymbol(),
-        internal,
-        node,
-        asType
-      );
-      mappedType.addGenericParameters(asType);
-      console.debug(`Adding mapped type ${name} to registry`);
-      this.registry.addType(mappedType);
-      return mappedType;
-    }
-    const regType = new TypeRegistryType(
-      this.registry,
-      name,
-      symbolToUse,
-      internal,
-      node,
-      asType
-    );
-    regType.addGenericParameters(asType);
-    const propertySignatures = asType.getApparentProperties();
-    propertySignatures.forEach((property) => {
-      const propertyName = property.getName();
-      const isOptional = property.isOptional();
-      const propertyType = property.getTypeAtLocation(node);
-      const isArray = propertyType.isArray();
-      const typeArgs = propertyType.getAliasTypeArguments();
-      const genericParameters = typeArgs.map(
-        (t) =>
-          t.getAliasSymbol()?.getName() ??
-          t.getSymbol()?.getName() ??
-          t.getText()
-      );
-      const propertyText = propertyType.getText();
-      const propertyTypeSymbol = propertyType.getSymbol();
-      const arrayElemType = propertyType.getArrayElementType();
-      const primitiveType = asPrimitiveTypeName(propertyType);
-      if (propertyType.isTypeParameter()) {
-        const genericParamName = propertyType.getSymbolOrThrow().getName();
-        regType.addProperty(
-          propertyName,
-          {
-            genericParamName,
-            isGenericReference: true,
-          },
-          {
-            isArray,
-            isOptional,
-            genericParameters,
-          }
-        );
-      } else if (primitiveType) {
-        const primitiveSymbol = this.registry
-          .getType(primitiveType)!
-          .getSymbol();
-        regType.addProperty(propertyName, primitiveSymbol, {
-          isArray,
-          isOptional,
-          genericParameters,
-        });
-      } else if (propertyTypeSymbol) {
-        if (isArray) {
-          const arrayElemTypeSymbol = getFinalSymbolOfType(
-            arrayElemType!.getApparentType()
-          );
-          const inRegistry =
-            arrayElemTypeSymbol && this.registry.getType(arrayElemTypeSymbol);
-          if (inRegistry) {
-            regType.addProperty(propertyName, arrayElemTypeSymbol, {
-              isArray,
-              isOptional,
-            });
-          } else {
-            console.debug(`Creating internal type ${name}${propertyName}Class`);
-            const anon = this.createType(
-              `${name}${propertyName}Class`,
-              node,
-              arrayElemType!,
-              true
-            );
-            regType.addProperty(propertyName, anon.getSymbol(), {
-              isArray,
-              isOptional,
-              genericParameters,
-            });
-          }
-        } else {
-          const inRegistry = this.registry.getType(propertyTypeSymbol);
-          if (!inRegistry) {
-            if (propertyType)
-              console.debug(
-                `Creating internal type ${name}${propertyName}Class`
-              );
-            const anon = this.createType(
-              `${name}${propertyName}Class`,
-              node,
-              propertyType,
-              true
-            );
-            regType.addProperty(propertyName, anon.getSymbol(), {
-              isArray,
-              isOptional,
-              genericParameters,
-            });
-          } else {
-            regType.addProperty(propertyName, propertyTypeSymbol, {
-              isArray,
-              isOptional,
-              genericParameters,
-            });
-          }
-        }
-      } else {
-        const fromText = this.registry.findTypeBySymbolText(propertyText);
-        if (fromText) {
-          regType.addProperty(propertyName, fromText.getSymbol(), {
-            isArray,
-            isOptional,
-            genericParameters,
-          });
-        } else {
-          if (propertyType) {
-            console.debug(`Creating internal type ${name}${propertyName}Class`);
-            const anon = this.createType(
-              `${name}${propertyName}Class`,
-              node,
-              propertyType,
-              true
-            );
-            regType.addProperty(propertyName, anon.getSymbol(), {
-              isArray,
-              isOptional,
-              genericParameters,
-            });
-          } else {
-            console.warn(
-              `No symbol for property ${propertyName} on type ${name}!`
-            );
-            const primitiveSymbol = this.registry
-              .getType("object")!
-              .getSymbol();
-            regType.addProperty(propertyName, primitiveSymbol, {
-              isArray,
-              isOptional,
-              genericParameters,
-            });
-          }
-        }
-      }
-    });
-    console.debug(`Adding type ${name} to registry`);
-    this.registry.addType(regType);
+    const options = { name, node, type: asType, internal };
+    const regType = this.typeFactory.createType(options);
     return regType;
   }
   private traverseNode(node: Node) {
---
+++ src/converter/type-factory.ts
@@ -0,0 +1,285 @@
+import { Node, Symbol, Type } from "ts-morph";
+import { TypeRegistry } from "./registry";
+import {
+  PropertyOptions,
+  TypeRegistryDictType,
+  TypeRegistryType,
+  TypeRegistryUnionType,
+} from "./registry-types";
+import { IRegistryType, PrimitiveTypeName } from "./types";
+import {
+  asPrimitiveTypeName,
+  createSymbol,
+  getFinalSymbolOfType,
+} from "./util";
+
+type TypeOptions = {
+  name: string;
+  node: Node;
+  type: Type;
+  internal: boolean;
+};
+type PropertyInfo = {
+  propertyName: string;
+  type: Type;
+  symbol?: Symbol;
+  arrayElemType?: Type;
+  primitiveType?: PrimitiveTypeName;
+  options: PropertyOptions;
+};
+
+export class TypeFactory {
+  constructor(private registry: TypeRegistry) {}
+  private getPropertyOptions(
+    parentNode: Node,
+    propertySymbol: Symbol
+  ): PropertyInfo {
+    const propertyName = propertySymbol.getName();
+    const isOptional = propertySymbol.isOptional();
+    const type = propertySymbol.getTypeAtLocation(parentNode);
+    const isArray = type.isArray();
+    const typeArgs = type.getAliasTypeArguments();
+    const genericParameters = typeArgs.map(
+      (t) =>
+        t.getAliasSymbol()?.getName() ?? t.getSymbol()?.getName() ?? t.getText()
+    );
+    const symbol = type.getSymbol();
+    const arrayElemType = type.getArrayElementType();
+    const primitiveType = asPrimitiveTypeName(type);
+    const options = { isArray, isOptional, genericParameters };
+    return {
+      propertyName,
+      type,
+      symbol,
+      arrayElemType,
+      primitiveType,
+      options,
+    };
+  }
+  private createPrimitiveType(options: TypeOptions): IRegistryType | undefined {
+    const { type } = options;
+    const typeAsPrimitive = asPrimitiveTypeName(type);
+    if (typeAsPrimitive) {
+      return this.registry.getType(typeAsPrimitive)!;
+    }
+    return;
+  }
+  private createUnionTypeFromUnion(
+    options: TypeOptions
+  ): IRegistryType | undefined {
+    const { name, node, type, internal } = options;
+    if (!type.isUnion()) {
+      return;
+    }
+    const symbolToUse = createSymbol(name, type);
+    const unionTypes = type.getUnionTypes();
+    const nonUndefinedUnionTypes = unionTypes.filter((u) => !u.isUndefined());
+    if (
+      nonUndefinedUnionTypes.every((unionType) => unionType.isStringLiteral())
+    ) {
+      const members = unionTypes.map((member) =>
+        member.getLiteralValueOrThrow()
+      );
+      const unionRegType = new TypeRegistryUnionType(
+        this.registry,
+        name,
+        symbolToUse,
+        members as string[],
+        internal,
+        type
+      );
+      return unionRegType;
+    }
+    if (nonUndefinedUnionTypes.length === 1) {
+      return this.createType({
+        name,
+        node,
+        type: nonUndefinedUnionTypes[0],
+        internal,
+      });
+    }
+    // TODO: handle other cases
+    return;
+  }
+  private createUnionTypeFromEnum(
+    options: TypeOptions
+  ): IRegistryType | undefined {
+    const { name, node, type, internal } = options;
+    if (type.isEnum()) {
+      console.debug("enum detected");
+    }
+    return;
+  }
+  private createMappedType(options: TypeOptions): IRegistryType | undefined {
+    const { name, node, type, internal } = options;
+    const symbolToUse = createSymbol(name, type);
+    const stringIndexType = type.getStringIndexType();
+    if (stringIndexType) {
+      const valueType = stringIndexType.getApparentType();
+      const indexType = this.registry.getType("string")!.getSymbol();
+      const valueTypeName = `${name}Value`;
+      console.debug(`Creating internal type ${valueTypeName}`);
+      const vType = this.createType({
+        name: valueTypeName,
+        node,
+        type: valueType,
+        internal: true,
+      });
+
+      const mappedType = new TypeRegistryDictType(
+        this.registry,
+        name,
+        symbolToUse,
+        indexType,
+        vType.getSymbol(),
+        internal,
+        node,
+        type
+      );
+      mappedType.addGenericParameters(type);
+      console.debug(`Adding mapped type ${name} to registry`);
+      return mappedType;
+    }
+    return;
+  }
+  private getFromRegistryOrCreateAnon(
+    node: Node,
+    type: Type,
+    name: string,
+    symbol?: Symbol
+  ): IRegistryType {
+    const fromRegistry = symbol && this.registry.getType(symbol);
+    if (fromRegistry) {
+      return fromRegistry;
+    }
+    const propertyText = type.getText();
+    const fromText = this.registry.findTypeBySymbolText(propertyText);
+    if (fromText) {
+      return fromText;
+    }
+    console.debug(`Creating internal type ${name}`);
+    const anon = this.createType({
+      name,
+      node,
+      type,
+      internal: true,
+    });
+    return anon;
+  }
+  private getPropertyTypeFromRegistry(
+    parentOptions: TypeOptions,
+    propertyInfo: PropertyInfo
+  ): IRegistryType {
+    const { node, name } = parentOptions;
+    const {
+      propertyName,
+      options,
+      type: propertyType,
+      symbol: propertyTypeSymbol,
+      arrayElemType,
+    } = propertyInfo;
+    const { isArray } = options;
+    const internalClassName = `${name}${propertyName}Class`;
+    if (isArray) {
+      const arrayElemTypeSymbol = getFinalSymbolOfType(
+        arrayElemType!.getApparentType()
+      )!;
+      return this.getFromRegistryOrCreateAnon(
+        node,
+        arrayElemType!,
+        internalClassName,
+        arrayElemTypeSymbol
+      );
+    }
+    return this.getFromRegistryOrCreateAnon(
+      node,
+      propertyType,
+      internalClassName,
+      propertyTypeSymbol
+    );
+  }
+  private handlePropertySignature(
+    registryType: TypeRegistryType,
+    parentOptions: TypeOptions,
+    property: Symbol
+  ) {
+    const { node } = parentOptions;
+    const propertyOptions = this.getPropertyOptions(node, property);
+    const {
+      propertyName,
+      options,
+      type: propertyType,
+      primitiveType,
+    } = propertyOptions;
+    if (propertyType.isTypeParameter()) {
+      const genericParamName = propertyType.getSymbolOrThrow().getName();
+      return registryType.addProperty(
+        propertyName,
+        {
+          genericParamName,
+          isGenericReference: true,
+        },
+        options
+      );
+    }
+    if (primitiveType) {
+      const primitiveSymbol = this.registry.getType(primitiveType)!.getSymbol();
+      return registryType.addProperty(propertyName, primitiveSymbol, options);
+    }
+    const propertyTypeFromRegistry = this.getPropertyTypeFromRegistry(
+      parentOptions,
+      propertyOptions
+    );
+    return registryType.addProperty(
+      propertyName,
+      propertyTypeFromRegistry.getSymbol(),
+      options
+    );
+  }
+
+  private createTypeOrInterfaceType(options: TypeOptions): IRegistryType {
+    const { name, node, type, internal } = options;
+    const symbolToUse = createSymbol(name, type);
+    const regType = new TypeRegistryType(
+      this.registry,
+      name,
+      symbolToUse,
+      internal,
+      node,
+      type
+    );
+    regType.addGenericParameters(type);
+    const propertySignatures = type.getApparentProperties();
+    propertySignatures.forEach((property) =>
+      this.handlePropertySignature(regType, options, property)
+    );
+    return regType;
+  }
+  private createTypeInternal(options: TypeOptions): IRegistryType {
+    const asPrimitive = this.createPrimitiveType(options);
+    if (asPrimitive) {
+      return asPrimitive;
+    }
+    const asUnionFromUnion = this.createUnionTypeFromUnion(options);
+    if (asUnionFromUnion) {
+      return asUnionFromUnion;
+    }
+    const asUnionFromEnum = this.createUnionTypeFromEnum(options);
+    if (asUnionFromEnum) {
+      return asUnionFromEnum;
+    }
+    const asMappedType = this.createMappedType(options);
+    if (asMappedType) {
+      return asMappedType;
+    }
+    const regType = this.createTypeOrInterfaceType(options);
+    return regType;
+  }
+  createType(options: TypeOptions): IRegistryType {
+    const regType = this.createTypeInternal(options);
+    const { tokenType, name } = regType.getStructure();
+    console.debug(`Adding ${tokenType} type ${name} to registry`);
+    this.registry.addType(regType);
+    return regType;
+  }
+}
